867d34f31a4ff056ffae0540e792e7b25d0d7775
SanjanaSK03 <01fe22bei004@kletech.ac.in>
2024-03-09 22:01:43 +0530

    Print corresponding word of the number

---
+++ PrintWord.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void printWord(int);
+void printWord(int a)
+{
+    switch(a)
+    {
+        case 1:printf("one");
+        break;
+        case 2:printf("two");
+        break;
+        case 3:printf("three");
+        break;
+        case 4:printf("four");
+        break;
+        case 5:printf("five");
+        break;
+        case 6:printf("six");
+        break;
+        case 7:printf("seven");
+        break;
+        case 8:printf("eight");
+        break;
+        case 9:printf("nine");
+        break;
+        default:printf("Greater than 9");
+        break;
+    }
+}
+int main()
+{
+    int num;
+    printf("Enter number=\n");
+    scanf("%d",&num);
+    printWord(num);
+}
